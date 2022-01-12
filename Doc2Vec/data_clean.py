a04d2cfd5c70e2da631c3be1105783f251aebc76
Roy Huang <46233972+f0r3usicsRoy@users.noreply.github.com>
2022-01-12 23:41:02 +0800

    Update data_clean.py

--- Doc2Vec/data_clean.py
+++ Doc2Vec/data_clean.py
@@ -7,9 +7,9 @@ import nltk
 from nltk.tokenize.toktok import ToktokTokenizer
 import re
 
-'''
-===============================消除stopword====================================
-'''
+
+#===============================消除stopword====================================
+
 
 tokenizer = ToktokTokenizer()
 stopword_list = nltk.corpus.stopwords.words('english')
@@ -23,11 +23,12 @@ def remove_stopwords(text, is_lower_case=False, stopwords=stopword_list):
     filtered_text = ' '.join(filtered_tokens)
     return filtered_text
 
-'''
-===============================詞型還原及資料清理===================================
-'''
+
+#===============================詞型還原及資料清理===================================
+
 def preprocess(text):
     # 詞型還原
+    spacy.require_cpu()
     nlp = spacy.load("en_core_web_sm")
     text = nlp(text)
     text = ' '.join([word.lemma_ if word.lemma_ != '-PRON-' else word.text for word in text])
@@ -42,20 +43,19 @@ def preprocess(text):
 
     return text
 
-'''
-===============================執行===================================
-'''
-# 設定路徑
-path = 'C:/Users/Roy/PycharmProjects/2021IR/sample/train_query/'
-files = os.listdir(path)
+#==============================執行doc資料前處理==========================
 
-df = pd.DataFrame(columns=['Number', 'text_body'])
+# 設定路徑
+path_doc = 'C:/Users/Roy/PycharmProjects/2021IR/sample/doc/'
+files_doc = os.listdir(path_doc)
+
+df_doc = pd.DataFrame(columns=['Number', 'text_body'])
 num = 0
 a = ''
 
-for f in tqdm(files):
+for f in tqdm(files_doc):
     # 匯入並解析 xml 檔案
-    with open(path + f, encoding="utf-8") as reader:
+    with open(path_doc + f, encoding="utf-8") as reader:
         xml = reader.read()
 
     # 移除 xml 所有標籤
@@ -64,12 +64,42 @@ for f in tqdm(files):
     text = preprocess(text)
 
     # 把 xml 資料加入 df 矩陣
-    df.loc[num] = [f, text]
+    df_doc.loc[num] = [f, text]
     a = (a + text + '\n')
 
     # 計數器 + 1
     num += 1
-with open("try.txt", "w+") as k:
+with open("train_query.txt", "w+", encoding='UTF-8') as k:
     k.write(a)
+df_doc.to_csv("train_query.csv")
 
-df.to_csv("df_result.csv")
+
+#===============================執行test資料前處理===================================
+
+# 設定路徑
+path_test = 'C:/Users/Roy/PycharmProjects/2021IR/sample/test_query/'
+files_test = os.listdir(path_test)
+
+df_test = pd.DataFrame(columns=['Number', 'text_body'])
+num = 0
+a = ''
+
+for f in tqdm(files_test):
+    # 匯入並解析 xml 檔案
+    with open(path_test + f, encoding="utf-8") as reader:
+        xml = reader.read()
+
+    # 移除 xml 所有標籤
+    soup = BeautifulSoup(xml, "html.parser")
+    text = soup.get_text()
+    text = preprocess(text)
+
+    # 把 xml 資料加入 df 矩陣
+    df_test.loc[num] = [f, text]
+    a = (a + text + '\n')
+
+    # 計數器 + 1
+    num += 1
+with open("test_query.txt", "w+", encoding='UTF-8') as k:
+    k.write(a)
+df_test.to_csv("test_query.csv")
